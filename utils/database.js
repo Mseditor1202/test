60672c04db60d72c4f061fb10ee584397d3aaa9e
Shota Morishita <webery.122@gmail.com>
2025-07-11 22:58:32 +0900

    First Commit!

---
+++ utils/database.js
@@ -0,0 +1,14 @@
+
+import mongoose from "mongoose"
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect("mongodb+srv://webery122:BNmXZidSUpD1RCyx@cluster0.enn79e6.mongodb.net/appDataBase?retryWrites=true&w=majority&appName=Cluster0")
+        console.log("Success:Connected to MongoDB")
+    }catch(err){
+        console.log("Failure:Connected to MongoDB")
+        throw new Error()
+    }
+}
+
+export default connectDB
