fb2cb45996fa6fdcbc7be45849aacf7462a1b283
Shota Morishita <webery.122@gmail.com>
2025-08-24 03:46:59 +0900

    next/headersの削除

--- pages/item/[id].js
+++ pages/item/[id].js
@@ -2,30 +2,32 @@ import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 import { getBaseUrl } from "../../lib/baseUrl";
-import { headers } from "next/headers";
-import { notFound } from "next/navigation";
 
-const ReadSingleItem = (props) => {
-  return (
+const ReadSingleItem = ({ singleItem }) => {
+  if (!singleItem) { 
+    return <div>アイテムが見つかりませんでした。</div>;
+  }
+  
+    return (
     <div className="grid-container-si">
-      <Head><title>{props.singleItem.title}</title></Head>
+      <Head><title>{singleItem.title}</title></Head>
       <div>
         <h1>個別アイテムページ</h1>
         <Image
-          src={props.singleItem.image}
+          src={singleItem.image}
           width={750}
           height={500}
           alt="item-image"
         />
       </div>
       <div>
-        <h1>{props.singleItem.title}</h1>
-        <h2>¥{props.singleItem.price}</h2>
+        <h1>{singleItem.title}</h1>
+        <h2>¥{singleItem.price}</h2>
         <hr/>
-        <p>{props.singleItem.description}</p>
+        <p>{singleItem.description}</p>
         <div>
-          <Link href={`/item/update/${props.singleItem._id}`}>アイテム編集</Link>
-          <Link href={`/item/delete/${props.singleItem._id}`}>アイテム削除</Link>
+          <Link href={`/item/update/${singleItem._id}`}>アイテム編集</Link>
+          <Link href={`/item/delete/${singleItem._id}`}>アイテム削除</Link>
         </div>
       </div>
     </div>
